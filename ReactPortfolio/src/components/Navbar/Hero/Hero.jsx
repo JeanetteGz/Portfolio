bf3bbe63aa7573236e9d2cddc2eea3c08d441d86
Jeanettegz <Jeanetteglz99@gmail.com>
2024-01-31 18:55:36 -0500

    My first commit

---
+++ ReactPortfolio/src/components/Navbar/Hero/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Hero = () => {
+  return <section>
+    <div>
+        <h1>Hi, I'm Jeanette!</h1>
+        <p>I am a front-end developer with expiernece using JavaScript, HTML, CSS. Checkout out my owrk below!</p>
+        <a href="mailto:yanette.cruzz@icloud.com"></a>
+    </div>
+  </section>
+};
+
